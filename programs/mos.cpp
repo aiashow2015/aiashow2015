55cd4f1fc627244e6b5e9c0a8e4a97f28b986f33
George Madrid <gmadrid@gmail.com>
2015-06-23 15:22:54 -0400

    Added twinkling.

--- programs/mos.cpp
+++ programs/mos.cpp
@@ -8,25 +8,33 @@ Cube cube;
 Seq* seq;
 
 static const Color OColor(0x9b, 0xb0, 0xff);
+static const Color ODark(0x74, 0x84, 0xbf);
 static const Color BColor(0xaa, 0xbf, 0xff);
+static const Color BDark(0x7f, 0x8f, 0xbf);
 static const Color AColor(0xca, 0xd7, 0xff);
+static const Color ADark(0x97, 0xa1, 0xbf);
 static const Color FColor(0xf8, 0xf7, 0xff);
+static const Color FDark(0xba, 0xb9, 0xbf);
 static const Color GColor(0xff, 0xf4, 0xea);
+static const Color GDark(0xbf, 0xb7, 0xaf);
 static const Color KColor(0xff, 0xd2, 0xa1);
+static const Color KDark(0xbf, 0x9d, 0x7a);
 static const Color MColor(0xff, 0xcc, 0x6f);
+static const Color MDark(0xbf, 0x99, 0x53);
 
 struct StarDesc {
-  const Color* color;   // Color to represent this type of star.
-  int percentage;       // Percentage of stars of this type (* 1000).
+  const Color* color;     // Color to represent this type of star.
+  const Color* darkColor; // Dark twinkle color.
+  int percentage;         // Percentage of stars of this type (* 1000).
 };
 
-static const StarDesc ODesc = { &OColor,   1 };
-static const StarDesc BDesc = { &BColor,   1 };
-static const StarDesc ADesc = { &AColor,   6 };
-static const StarDesc FDesc = { &FColor,  30 };
-static const StarDesc GDesc = { &GColor,  76 };
-static const StarDesc KDesc = { &KColor, 121 };
-static const StarDesc MDesc = { &MColor, 765 };
+static const StarDesc ODesc = { &OColor, &ODark,   1 };
+static const StarDesc BDesc = { &BColor, &BDark,   1 };
+static const StarDesc ADesc = { &AColor, &ADark,   6 };
+static const StarDesc FDesc = { &FColor, &FDark,  30 };
+static const StarDesc GDesc = { &GColor, &GDark,  76 };
+static const StarDesc KDesc = { &KColor, &KDark, 121 };
+static const StarDesc MDesc = { &MColor, &MDark, 765 };
 
 static const StarDesc *descs[] {
   &MDesc, &KDesc, &GDesc, &FDesc, &ADesc, &BDesc, &ODesc
@@ -44,6 +52,7 @@ public:
       stars[i].x = rand()%8;
       stars[i].y = rand()%8;
       stars[i].z = rand()%8;
+      stars[i].twinkle = twinkle();
 
       int randType = rand()%1000;
       int len = sizeof(descs)/sizeof(*descs);
@@ -60,22 +69,31 @@ public:
 
     cube.background(black);    
   }
+
+  bool twinkle() {
+    return rand() % 100 == 1;
+  }
   
   virtual void loop(long elapsed, double percentDone) {
     for (int i = 0; i < NUM_STARS; ++i) {
       star* s = &stars[i];
-      cube.setVoxel(s->x, s->y, s->z, *s->desc->color);
+      if (twinkle()) {
+	s->twinkle = !s->twinkle;
+      }
+      const Color* color = s->twinkle ? s->desc->darkColor : s->desc->color;
+      cube.setVoxel(s->x, s->y, s->z, *color);
     }
     cube.show();
   }
 private:
-  static const int NUM_STARS = 50;
+  static const int NUM_STARS = 75;
   
   struct star {
     const StarDesc* desc;
     int x;
     int y;
     int z;
+    int twinkle;
   };
 
   star stars[NUM_STARS];
@@ -228,7 +246,7 @@ public:
 };
 
 Task* tasks[] = {
-  new StarField(600),
+  new StarField(6000),
     // new BlueInCenter(200),
     // new BlueLine(300),
     // new BlueSquare(150),
