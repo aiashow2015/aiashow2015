b4f4acb9463dac6c94c0085dcd9228911c1038cd
George Madrid <gmadrid@gmail.com>
2015-06-23 14:29:48 -0400

    Adding MoS starfield.

--- programs/mos.cpp
+++ programs/mos.cpp
@@ -1,9 +1,85 @@
+#include <cstdlib>
+
 #include "application.h"
 #include "beta-cube-library.h"
 #include "seq.h"
 
 Cube cube;
 Seq* seq;
+
+static const Color OColor(0x9b, 0xb0, 0xff);
+static const Color BColor(0xaa, 0xbf, 0xff);
+static const Color AColor(0xca, 0xd7, 0xff);
+static const Color FColor(0xf8, 0xf7, 0xff);
+static const Color GColor(0xff, 0xf4, 0xea);
+static const Color KColor(0xff, 0xd2, 0xa1);
+static const Color MColor(0xff, 0xcc, 0x6f);
+
+struct StarDesc {
+  const Color* color;   // Color to represent this type of star.
+  int percentage;       // Percentage of stars of this type (* 1000).
+};
+
+static const StarDesc ODesc = { &OColor,   1 };
+static const StarDesc BDesc = { &BColor,   1 };
+static const StarDesc ADesc = { &AColor,   6 };
+static const StarDesc FDesc = { &FColor,  30 };
+static const StarDesc GDesc = { &GColor,  76 };
+static const StarDesc KDesc = { &KColor, 121 };
+static const StarDesc MDesc = { &MColor, 765 };
+
+static const StarDesc *descs[] {
+  &MDesc, &KDesc, &GDesc, &FDesc, &ADesc, &BDesc, &ODesc
+};
+
+class StarField : public Task {
+public:
+  StarField(long delayMillis) : Task("starfield", delayMillis) {
+  }
+
+  virtual void setup(long startTimeMillis) {
+    Task::setup(startTimeMillis);
+
+    for (int i = 0; i < NUM_STARS; ++i) {
+      stars[i].x = rand()%8;
+      stars[i].y = rand()%8;
+      stars[i].z = rand()%8;
+
+      int randType = rand()%1000;
+      int len = sizeof(descs)/sizeof(*descs);
+      for (int j = 0; j < len; ++j) {
+	const StarDesc* desc = descs[j];
+	if (randType < desc->percentage || j == len - 1) {
+	  stars[i].desc = desc;
+	  break;
+	} else {
+	  randType -= desc->percentage;
+	}	
+      }
+    }
+
+    cube.background(black);    
+  }
+  
+  virtual void loop(long elapsed, double percentDone) {
+    for (int i = 0; i < NUM_STARS; ++i) {
+      star* s = &stars[i];
+      cube.setVoxel(s->x, s->y, s->z, *s->desc->color);
+    }
+    cube.show();
+  }
+private:
+  static const int NUM_STARS = 50;
+  
+  struct star {
+    const StarDesc* desc;
+    int x;
+    int y;
+    int z;
+  };
+
+  star stars[NUM_STARS];
+};  
 
 class RedLight : public Task {
 public:
@@ -152,15 +228,17 @@ public:
 };
 
 Task* tasks[] = {
-    new BlueInCenter(2000),
-    new BlueLine(300),
-    new BlueSquare(150),
-    new DelayTask(150),
-    new BlueCube(150),
-    new YellowLight(100),
-    new DelayTask(50),
-    new WhiteCube(300),
-    new ClearTask(10),
+  new StarField(600),
+    // new BlueInCenter(200),
+    // new BlueLine(300),
+    // new BlueSquare(150),
+    // new DelayTask(150),
+    // new BlueCube(150),
+    // new YellowLight(100),
+    // new DelayTask(50),
+    // new WhiteCube(300),
+    // new ClearTask(10),
+
     // The nullptr means that the sequence is done.
     // Don't forget it, or it will crash!!!
     nullptr
